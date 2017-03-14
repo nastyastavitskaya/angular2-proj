b9ed129bd876ec1b6c3707fcaad5825635b0a18b
Nastya Stavitskaya <nastycorps@gmail.com>
2017-03-14 14:55:31 -0700

    Add layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ProductsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
