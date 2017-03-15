08fbab348b2aa421f5b4498881b9db6bcdcd19c2
Nastya Stavitskaya <nastycorps@gmail.com>
2017-03-15 16:53:54 -0700

    Add hello popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+import { AlertModule } from 'ng2-bootstrap/alert';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -24,7 +25,8 @@ import { APP_ROUTES } from './app.routes'
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(APP_ROUTES)
+    RouterModule.forRoot(APP_ROUTES),
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
