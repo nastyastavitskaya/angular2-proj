eec5705784c16e264ec96c51164dc5cac621e9a8
Nastya Stavitskaya <nastycorps@gmail.com>
2017-03-16 17:59:00 -0700

    Add products data mock

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 var
   express = require('express')
   app = express()
+  products = require('./mock.products.json')
 
 app.get('/api/test', function (req, res) {
   res.send('API server works!')
+})
+
+app.get('/api/products', function (req, res) {
+  res.setTimeout(1500, function(){
+    res.send(products)
+  })
 })
 
 app.listen(3000, function () {
