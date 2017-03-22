adde2a07e73db4d68bdf9fc6f5184a2a82b773fb
Nastya Stavitskaya <nastycorps@gmail.com>
2017-03-21 20:18:14 -0700

    Add products Pre-Loader

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -11,13 +11,19 @@ import { Product } from 'app/product.model';
 })
 export class ProductsComponent implements OnInit {
   private products: Product[]=[];
+  pending:boolean = false;
 
   constructor(private productsService: ProductsService) {}
 
   ngOnInit() { this.getProducts(); }
 
   getProducts() {
+    this.pending = true;
     this.productsService.getProducts()
-                        .subscribe(products => this.products = products)
+                        .subscribe(products => {
+                                      this.products = products;
+                                      this.pending = false;
+                                    },
+                                    error => {this.pending = false;})
   }
 }
