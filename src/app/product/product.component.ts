b96f4844b9d3154708fc07023f4888a077bec717
Nastya Stavitskaya <nastycorps@gmail.com>
2017-03-23 18:35:06 -0700

    Edit product component template

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,27 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ProductsService } from 'app/products.service';
 
 import { Product } from 'app/product.model';
 
 @Component({
   selector: 'app-product',
-  providers: [ProductsService]
+  templateUrl: './product.component.html'
 })
-export class ProductComponent implements OnInit {
-  @Input() products: Product[];
+export class ProductComponent {
+  @Input() product: Product[];
   @Input() pending:boolean;
-
-  constructor(private productsService: ProductsService) {}
-
-  ngOnInit() { this.getProducts(); }
-
-  getProducts() {
-      this.pending = true;
-      this.productsService.getProducts()
-                          .subscribe(products => {
-                                        this.products = products;
-                                        this.pending = false;
-                                      },
-                                      error => {this.pending = false;})
-  }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'app/products.service';
+
 
 import { Product } from 'app/product.model';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
+  providers: [ProductsService]
 })
-export class ProductsComponent {
-  products: Product[]=[];
+export class ProductsComponent implements OnInit {
+  products=[];
   pending:boolean = false;
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit() { this.getProducts(); }
+
+  getProducts() {
+      this.pending = true;
+      this.productsService.getProducts()
+                          .subscribe(products => {
+                                        this.products = products;
+                                        this.pending = false;
+                                      },
+                                      error => {this.pending = false;})
+  }
 }
