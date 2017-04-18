8e7b436dcc8184665d35a8ce17860547dd518de9
Nastya Stavitskaya <nastycorps@gmail.com>
2017-04-17 19:10:25 -0700

    Cart Product Quantity Observe. Header fix

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -9,11 +9,9 @@ export class ShoppingCartService {
   private cartProducts: CartProduct[] = [];
   private totalPrice: number;
 
-  private totalQuantity: number = 0;
+  private totalQuantity: number;
   private productsQuantitySource = new Subject<any>();
-
   changeProductsQuantity$ = this.productsQuantitySource.asObservable();
-
 
   addProduct(product: Product){
    let p = this.cartProducts.find(cartProduct => cartProduct.product.id === product.id);
@@ -23,12 +21,9 @@ export class ShoppingCartService {
       console.log('already in cart:', p.product.name, p.quantity);
     }
     else {
-      let cartProduct = new CartProduct(product, 1);
-      this.cartProducts.push(cartProduct);
-      console.log('added in cart:', cartProduct.product.name, cartProduct.quantity);
+      this.addNewCartProduct(product)
     }
     this.setTotalPrice();
-    this.productsQuantitySource.next(++this.totalQuantity);
   }
 
   getProducts(){
@@ -38,6 +33,20 @@ export class ShoppingCartService {
 
   getTotalPrice(){
     return this.totalPrice;
+  }
+
+  private addNewCartProduct(product: Product){
+    let newCartProduct = new CartProduct(product, 1);
+    this.cartProducts.push(newCartProduct);
+    newCartProduct.quantity$.subscribe(_quantity => this.setTotalQuantity());
+  }
+
+  private setTotalQuantity() {
+    this.totalQuantity = this.cartProducts.reduce((prev, curr: CartProduct) => {
+      return prev + curr.quantity;
+    }, 0);
+    this.productsQuantitySource.next(this.totalQuantity);
+    console.log('Total quantity:', this.totalQuantity);
   }
 
   private setTotalPrice(){
--- src/app/shopping-cart/cart-product.model.ts
+++ src/app/shopping-cart/cart-product.model.ts
@@ -1,11 +1,27 @@
 import { Product } from 'app/product.model';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 export class CartProduct {
-  public product: Product;
-  public quantity: number;
+  product: Product;
 
-  constructor(product: Product, quantity: number){
+  private _quantity: number;
+
+  set quantity(_quantity: number) {
+    this._quantitySource.next(_quantity);
+    console.log('Quantity changed', _quantity);
+  }
+
+  get quantity():number {
+    return this._quantitySource.getValue();
+  }
+
+  private _quantitySource = new BehaviorSubject<number>(0);
+  quantity$ = this._quantitySource.asObservable();
+
+  constructor(product: Product, quantity: number) {
     this.product = product;
     this.quantity = quantity;
   }
 }
+
+
